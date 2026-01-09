bf9958f3abc285e13bfc294552789187043855fb
carlosjrs006 <carlosjrs06.cj@gmail.com>
2026-01-09 12:26:01 -0300

    feat: estrutura inicial do mini-autorizador

---
+++ src/test/java/com/teste/cliente/vr/mini_autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.cliente.vr.mini_autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
