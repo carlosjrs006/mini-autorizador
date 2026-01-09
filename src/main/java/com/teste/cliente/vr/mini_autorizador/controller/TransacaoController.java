aa21de261f328f3519bc23140c4a22b5fc14fc89
carlosjrs006 <carlosjrs06.cj@gmail.com>
2026-01-09 13:56:44 -0300

    Feat: criacao do controller responsavel por gerir as transacoes de cartoes.

---
+++ src/main/java/com/teste/cliente/vr/mini_autorizador/controller/TransacaoController.java
@@ -0,0 +1,26 @@
+package com.teste.cliente.vr.mini_autorizador.controller;
+
+import com.teste.cliente.vr.mini_autorizador.dto.TransacaoDTO;
+import com.teste.cliente.vr.mini_autorizador.service.AutorizadorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacoes")
+@RequiredArgsConstructor
+public class TransacaoController {
+
+    private final AutorizadorService autorizadorService;
+
+    @PostMapping
+    public ResponseEntity<String> realizarTransacao(@Valid @RequestBody TransacaoDTO transacaoDTO) {
+        autorizadorService.processarTransacao(transacaoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Transação realizada!!!");
+    }
+}
